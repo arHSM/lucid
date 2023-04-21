b0622fd811c32a724faaed7cce6a481726cff222
Han Seung Min - 한승민 <hanseungmin.ar@gmail.com>
2023-04-22 04:35:15 +0900

    feat: docs logo

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/arHSM/lucid/main/assets/logo.png",
+    html_favicon_url = "https://raw.githubusercontent.com/arHSM/lucid/main/assets/logo.png",
+    html_no_source
+)]
